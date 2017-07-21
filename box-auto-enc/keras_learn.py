a2e099c5d4c4400410765d03b722dfcafa44a9ac
zero-impact <lawsonfulton@gmail.com>
2017-07-21 14:13:41 -0400

    Tweaking keras still with 8 units. added notebook

--- box-auto-enc/keras_learn.py
+++ box-auto-enc/keras_learn.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,15 +9,18 @@ import keras
 from keras.layers import Input, Dense
 from keras.models import Model
 
-def rand_box(x_bound, y_bound, size=1.0):
+# TODO make sure nothing is generate out of bounds
+def rand_box(x_bounds, y_bounds, size=2.0):
     """ Generates a list of 4 points corresponding to a box at a random location
-    and orientation within the square defined by (-x_bound,-ybound) and (x_bound,y_bound)"""
-    box = np.array([[-1, 1], [1, 1], [1, -1], [-1, -1]])
+    and orientation within the square defined by x_bounds and y_bounds with side length size"""
+    half_size = size / 2.0
+    box = np.array([[-half_size, half_size], [half_size, half_size], [half_size, -half_size], [-half_size, -half_size]])
 
+    bound_buffer = math.sqrt(2 * (size / 2.0) ** 2) # Squares rotate...
     theta = random.uniform(0, math.pi * 2)
     offset = np.array([
-        random.uniform(x_bound[0] + size, x_bound[1] - size),
-        random.uniform(y_bound[0] + size, y_bound[1] - size)
+        random.uniform(x_bounds[0] + bound_buffer, x_bounds[1] - bound_buffer),
+        random.uniform(y_bounds[0] + bound_buffer, y_bounds[1] - bound_buffer)
     ])
 
     rotMatrix = np.array(
@@ -41,6 +45,7 @@ def generate_box_samples(sample_size, x_bounds, y_bounds):
     return np.array([box_to_vec(rand_box(x_bounds, y_bounds)) for _ in range(sample_size)])
 
 def main():
+    start_time = time.time()
     # Setup matplotlib
     x_bounds = [-10, 10]
     y_bounds = [-10, 10]
@@ -55,16 +60,17 @@ def main():
     ax.set_aspect('equal')
 
     # Setup and train the neural net
-    training_sample_size = 100000
-    test_sample_size = 100
+    training_sample_size = 1000000
+    test_sample_size = 1000
 
     print("Generating training data...")
     # TODO This could be more efficient
     train_data = generate_box_samples(training_sample_size, x_bounds, y_bounds)
     test_data = generate_box_samples(test_sample_size, x_bounds, y_bounds)
     # TODO test data should be from a different part of the plane to make sure we are generalizing
-    print("Done.")
+    print("Done. Runtime: ", time.time()-start_time)
     
+    model_start_time = time.time()
     # this is the size of our encoded representations
     encoding_dim = 8
 
@@ -81,13 +87,15 @@ def main():
     decoded = Dense(8, activation='relu',kernel_initializer=initializer)(encoded)
 
     autoencoder = Model(input_vec, decoded)
+
+    #### TODO 0.001 or 0.01???
     optimizer = keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     autoencoder.compile(optimizer=optimizer, loss='mean_squared_error')
 
     #train
     autoencoder.fit(train_data, train_data,
                 epochs=10,
-                batch_size=256,#512,
+                batch_size=1024,
                 shuffle=True,
                 validation_data=(test_data, test_data))
 
@@ -96,10 +104,19 @@ def main():
     # note that we take them from the *test* set
     decoded_boxes = autoencoder.predict(test_data)
 
+    print("Total model time: ", time.time() - model_start_time)
+    print("Total runtime: ", time.time() - start_time)
+
     # Draw some output
     for i in range(10):
         draw_box(box_from_vec(test_data[i]), ax, 'r', linewidth=5)
         draw_box(box_from_vec(decoded_boxes[i]), ax, 'b')
+
+    for i in range(len(test_data)):
+        if any(c > 1.0 or c < -1.0 for c in test_data[i]):
+            print("Bad test data: ", box_from_vec(test_data[i]))
+        if any(c > 1.0 or c < -1.0 for c in decoded_boxes[i]):
+            print("Bad predicted data: ", box_from_vec(decoded_boxes[i]))
 
     plt.show()
 
